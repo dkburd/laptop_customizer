ae74b91d405f44aed5f4291a6e31ab476d87dca1
Dana Burd <burd.dana@gmail.com>
2021-05-23 18:03:39 -0700

    header and cart

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
-import slugify from 'slugify';
+import slugify from "slugify";
 
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Cart from "./Cart";
 
 // This object will allow us to
 // easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
 });
 
 class App extends Component {
   state = {
     selected: {
       Processor: {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
-        cost: 700
+        name: "17th Generation Intel Core HB (7 Core with donut spare)",
+        cost: 700,
       },
-      'Operating System': {
-        name: 'Ubuntu Linux 16.04',
-        cost: 200
+      "Operating System": {
+        name: "Ubuntu Linux 16.04",
+        cost: 200,
       },
-      'Video Card': {
-        name: 'Toyota Corolla 1.5v',
-        cost: 1150.98
+      "Video Card": {
+        name: "Toyota Corolla 1.5v",
+        cost: 1150.98,
       },
       Display: {
         name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
-      }
-    }
+        cost: 1500,
+      },
+    },
   };
 
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
     this.setState({
-      selected
+      selected,
     });
   };
 
   render() {
     const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
+      const featureHash = feature + "-" + idx;
+      const options = this.props.features[feature].map((item) => {
         const itemHash = slugify(JSON.stringify(item));
         return (
           <div key={itemHash} className="feature__item">
@@ -56,7 +58,7 @@ class App extends Component {
               className="feature__option"
               name={slugify(feature)}
               checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
+              onChange={(e) => this.updateFeature(feature, item)}
             />
             <label htmlFor={itemHash} className="feature__label">
               {item.name} ({USCurrencyFormat.format(item.cost)})
@@ -76,7 +78,7 @@ class App extends Component {
     });
 
     const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
+      const featureHash = feature + "-" + idx;
       const selectedOption = this.state.selected[feature];
 
       return (
@@ -97,15 +99,17 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header>
+        {/* <header>
           <h1>ELF Computing | Laptops</h1>
-        </header>
+        </header> */}
+        <Header />
         <main>
           <form className="main__form">
             <h2>Customize your laptop</h2>
             {features}
           </form>
-          <section className="main__summary">
+          {/* section moved to cart:  */}
+          {/* <section className="main__summary">
             <h2>Your cart</h2>
             {summary}
             <div className="summary__total">
@@ -114,7 +118,8 @@ class App extends Component {
                 {USCurrencyFormat.format(total)}
               </div>
             </div>
-          </section>
+          </section> */}
+          <Cart selected={this.state.selected} />
         </main>
       </div>
     );
